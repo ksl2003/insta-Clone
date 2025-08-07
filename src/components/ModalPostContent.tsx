573a3bc3c5cd1e2186431d40b297ab98488cacc1
ksl2003 <ksuryalakshman5@gmail.com>
2025-08-07 20:06:01 +0530

    solved errors

--- src/components/ModalPostContent.tsx
+++ src/components/ModalPostContent.tsx
@@ -1,11 +1,18 @@
-import {getSinglePostData} from "@/actions";
+import { getSinglePostData } from "@/actions";
 import SinglePostContent from "@/components/SinglePostContent";
+import React from "react";
 
-export default async function ModalPostContent({postId}:{postId:string}) {
-  const {
-    post, authorProfile, comments,
-    commentsAuthors, myLike, myBookmark,
-  } = await getSinglePostData(postId);
+export default async function ModalPostContent({ postId }: { postId: string }) {
+  const data = await getSinglePostData(postId);
+  if (!data) {
+    return (
+      <div className="text-center py-20 text-lg text-muted-foreground">
+        Post not found.
+      </div>
+    );
+  }
+  const { post, authorProfile, comments, commentsAuthors, myLike, myBookmark } =
+    data;
   return (
     <SinglePostContent
       post={post}
